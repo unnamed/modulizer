4210de8a8251d01d17ccfb602b17d6e402e9c2de
ImGalaxy977 <amiranda8695@gmail.com>
2020-09-08 18:03:20 -0500

    Starting with the bukkit-wrappers.

---
+++ bukkit/src/main/java/team/unnamed/modulizer/bukkit/bind/VersionModuleBinder.java
@@ -0,0 +1,7 @@
+package team.unnamed.modulizer.bukkit.bind;
+
+import team.unnamed.modulizer.bukkit.MinecraftVersion;
+import team.unnamed.modulizer.universal.internal.SimpleModuleBinder;
+
+public final class VersionModuleBinder extends SimpleModuleBinder<MinecraftVersion> {
+}
---
+++ bukkit/src/main/java/team/unnamed/modulizer/bukkit/module/VersionModule.java
@@ -0,0 +1,6 @@
+package team.unnamed.modulizer.bukkit.module;
+
+import team.unnamed.modulizer.bukkit.MinecraftVersion;
+import team.unnamed.modulizer.universal.SimpleModule;
+
+public interface VersionModule extends SimpleModule<MinecraftVersion> { }
