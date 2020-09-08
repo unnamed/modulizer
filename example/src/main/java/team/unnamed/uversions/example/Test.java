656417bfd9222fbf2a589720451791e4dfa9a96d
ImGalaxy977 <amiranda8695@gmail.com>
2020-09-08 18:03:32 -0500

    Example to bukkit-wrappers.

--- example/src/main/java/team/unnamed/uversions/example/Test.java
+++ example/src/main/java/team/unnamed/uversions/example/Test.java
@@ -1,18 +1,18 @@
 package team.unnamed.uversions.example;
 
-import team.unnamed.modulizer.universal.bind.InternalModuleBinder;
-import team.unnamed.modulizer.universal.internal.SimpleModuleBinder;
+import team.unnamed.modulizer.bukkit.MinecraftVersion;
+import team.unnamed.modulizer.bukkit.bind.VersionModuleBinder;
 
 public class Test {
 
     public static void main(String[] args) {
-        InternalModuleBinder binder = new SimpleModuleBinder();
+        VersionModuleBinder binder = new VersionModuleBinder();
 
         binder.installModule(new TestModule());
 
         System.out.println(
                 binder
-                        .getProvider(Abstraction.class, MinecraftVersion.v1_8_R1)
+                        .getProvider(Abstraction.class)
                         .getInstance(MinecraftVersion.v1_8_R1, null,"simple-constructor", "testing", 2)
                         .getName()
         );
--- example/src/main/java/team/unnamed/uversions/example/TestModule.java
+++ example/src/main/java/team/unnamed/uversions/example/TestModule.java
@@ -1,15 +1,16 @@
 package team.unnamed.uversions.example;
 
 
-import team.unnamed.modulizer.universal.SimpleModule;
+import team.unnamed.modulizer.bukkit.MinecraftVersion;
+import team.unnamed.modulizer.bukkit.module.VersionModule;
 import team.unnamed.modulizer.universal.bind.ModuleBinder;
 
-public class TestModule implements SimpleModule {
+public class TestModule implements VersionModule {
 
     @Override
-    public void configure(ModuleBinder binder) {
+    public void configure(ModuleBinder<MinecraftVersion> binder) {
         binder
-                .bind(Abstraction.class, MinecraftVersion.class)
+                .bind(Abstraction.class)
                 .to(Implementation.class)
                 .withDefaultConstructor()
                 .withConstructor("simple-constructor", String.class, int.class)
